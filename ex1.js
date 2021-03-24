b41efbace226882328d5c9aef7fcf98f97aeb34a
Juan Pablo Kirpach <jpkirpach@gmail.com>
2021-03-24 17:20:42 -0300

    BQA-08: First exercise

---
+++ ex1.js
@@ -0,0 +1,23 @@
+//Exercise 1 - Variables & Operators
+//a
+var firstNumber = 10, secondNumber = 15;
+
+var sumNumbers = firstNumber + secondNumber;
+
+console.log(sumNumbers);
+
+//b
+var firstString = 'Radium', secondString = 'Rocket';
+
+var concatStrings = firstString + secondString;
+
+console.log(concatStrings);
+
+//c
+var thirdString = 'Juan', fourthString = 'Pablo';
+var sumLength = thirdString.length + fourthString.length;
+console.log(sumLength);
+
+
+
+
